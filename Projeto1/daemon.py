83ca7eadd8c547ccc8be0e3a984f8bdd4ed915b9
Camilo Moreira <camilomoreira91@gmail.com>
2015-10-02 11:08:09 -0300

    Corrige comandos que usam ;<>|& no bash do daemon; Corrige número de comandos inválidos no request (RESPOND 0) Adiciona timeout com conexao com o daemon. Socket da thread dura até 60 segundos e precisa ser reaberto. Adiciona possibilidade de um mesmo socket receber diversas REQUESTs Corrige outros problemas

--- Projeto1/daemon.py
+++ Projeto1/daemon.py
@@ -1,46 +1,75 @@
 operacao = ""
 
-def executestr(checkstr):
-	if (checkstr[0:7] == "REQUEST"):
-		global operacao
-		comando = checkstr[8:]
-		comando = comando.replace(' ',' -')
-		comando = comando.split()
+'''
+Funcao executestr(checkstr)
+'''
+
+def parse_and_execute(sentence):
+	global operacao
+
+	if (sentence[0:7] == "REQUEST"):
+		saidaValida = True
+		comando = sentence[8:]
+
 		if (comando[0] == "1"):
 			operacao = "1 "
-			comando.pop(0)
-			comando.insert(0,"ps")
+			comando = comando.replace("1","ps",1)
 		elif (comando[0] == "2"):
 			operacao = "2 "
-			comando.pop(0)
-			comando.insert(0,"df")
+			comando = comando.replace("2","df",1)
 		elif (comando[0] == "3"):
 			operacao = "3 "
-			comando.pop(0)
-			comando.insert(0,"finger")
+			comando = comando.replace("3","finger",1)
 		elif (comando[0] == "4"):
 			operacao = "4 "
-			comando.pop(0)
-			comando.insert(0,"uptime")
-		saida = subprocess.check_output(comando)
-		return saida
+			comando = comando.replace("4","uptime",1)
+		else:
+			operacao = "0"
+			saidaValida = False
+
+		if(saidaValida):
+			comando = cleanStr(comando)
+			comando = comando.replace(' ',' -',1)
+			comando = comando.split()
+			print comando
+			saida = subprocess.check_output(comando)
+			return saida
+		else:
+			return ''
 	else:
-		return "erro no REQUEST".encode()
+		operacao = "0"
+		return ''
 
 def cleanStr(stringUser):
-	string = stringUser.replace('>',' ')
-	string = string.replace('|',' ')
-	clearStr = string.replace(';',' ')
-	return clearStr
+	erro = len(stringUser)
+	outro_erro = stringUser.find('>')
+	if(outro_erro > 0):
+		erro = min(erro,outro_erro)
+	outro_erro = stringUser.find('<')
+	if(outro_erro > 0):
+		erro = min(erro,outro_erro)
+	outro_erro = stringUser.find('|')
+	if(outro_erro > 0):
+		erro = min(erro,outro_erro)
+	outro_erro = stringUser.find('&')
+	if(outro_erro > 0):
+		erro = min(erro,outro_erro)
+	outro_erro = stringUser.find(';')
+	if(outro_erro > 0):
+		erro = min(erro,outro_erro)
+	return stringUser[0:erro]
 
-def threadfunction(connectionSocket):
-	sentence = connectionSocket.recv(10000)
-	strlimpa = cleanStr(sentence.decode())
-	strlimpa
-	comandoexecutado = executestr(strlimpa)
-	msgfinal = "RESPOND " + operacao + comandoexecutado.decode()
-	connectionSocket.send(msgfinal.encode())
-	connectionSocket.close()
+def threadfunction(connectionSocket):	
+	connectionSocket.settimeout(21)
+	try:
+		while 1:
+			sentence = connectionSocket.recv(10000)
+			print sentence
+			comando_executado = parse_and_execute(sentence)
+			msgfinal = "RESPOND " + operacao + comando_executado.decode()
+			connectionSocket.send(msgfinal.encode())
+	except timeout:
+		connectionSocket.close()
 
 #main
 from socket import *
@@ -51,6 +80,7 @@ serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
+#serverSocket.settimeout(60)
 print ("The server is ready to receive")
 while 1:
 	connectionSocket, addr = serverSocket.accept()
