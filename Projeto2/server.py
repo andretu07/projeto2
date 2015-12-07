f7338dc1ec54715f7f37e8655ff8b4a563c118e2
andre <andretu07@gmail.com>
2015-12-06 23:14:10 -0200

    alterações

--- Projeto2/server.py
+++ Projeto2/server.py
@@ -1,3 +1,8 @@
+﻿import threading
+import time
+from socket import *
+from subprocess import *
+
 def createPackage(seqAckNumber, checksum, finbit, data):
 	return "+++".join((str(seqAckNumber), str(checksum), str(finbit), data))
 
@@ -5,28 +10,40 @@ def getFieldPackage(package, number):
 	field = package.split("+++")
 	return field[number]
 
-from socket import *
-"""
-N  = window size
-Rn = request number
-Sn = sequence number
-Sb = sequence base
-Sm = sequence max
+def listener(serverSocket):
+	global requestNumber
+	global base
+	serverSocket.settimeout(60)
+	try:
+		while 1:
+			response, clientAddress = serverSocket.recvfrom(2048)
+			base = requestNumber + 1
+	except timeout:
+		serverSocket.close()
+	#Erro na decodificação do unicode. Ignorar.
+	except:
+		None
 
-Sender:
-Sb = 0
-Sm = N − 1
-Repeat the following steps forever:
-1. If you receive a request number where Rn > Sb
-        Sm = Sm + (Rn − Sb)
-        Sb = Rn
-2.  If no packet is in transmission, 
-        Transmit a packet where Sb <= Sn <= Sm.  
-        Packets are transmitted in order.
-"""
+def burst(serverSocket):
+	global base
+	global requestNumber
+	counter = base
+	serverSocket.settimeout(60)
+	try:
+		time.sleep(1)
+		while counter != requestNumber - 1:
+			serverSocket.sendto(packages[counter], clientAddress)	
+			counter += 1
+	except timeout:
+		serverSocket.close()
+	#Erro na decodificação do unicode. Ignorar.
+	except:
+		None
+
 N = 6
-seqBase = 0
-seqMax = N - 1
+base = 0
+requestNumber = 0
+packages = []
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
@@ -42,18 +59,26 @@ while 1:
 		package = createPackage(0, 0, 0, "404 Not Found")
 		serverSocket.sendto(package.encode(), clientAddress)
 	else:
-		package = createPackage(0, 0, 0, "200 OK")
-		serverSocket.sendto(package.encode(), clientAddress)
+		count = 0
 		with open(fileToOpen, "rb") as f:
 			for chunk in iter(lambda: f.read(1024), b""):
-				if seqBase == requestNumber:
-					package = createPackage(requestNumber, 0, 0, chunk.decode("ISO-8859-1"))
-					serverSocket.sendto(package.encode(), clientAddress)
-					seqBase += 1					
-					requestNumber += 1
-					print("Enviando pacote " + str(requestNumber))
-					response, clientAddress = serverSocket.recvfrom(2048)
-					requestNumber = int(getFieldPackage(response.decode(), 0))
+				package = createPackage(count, 0, 0, chunk.decode("ISO-8859-1"))
+				packages.append(package.encode())
+				count += 1
+		t1 = threading.Thread(target=listener, args=(serverSocket, ))
+		t1.daemon = True
+		t1.start()
+		t2 = threading.Thread(target=burst, args=(serverSocket, ))
+		t2.daemon = True
+		t2.start()
+		package = createPackage(0, 0, 0, "200 OK")
+		serverSocket.sendto(package.encode(), clientAddress)
+		while requestNumber < len(packages):
+			if requestNumber < base + N:
+				print("Enviando pacote " + str(requestNumber))
+				serverSocket.sendto(packages[requestNumber], clientAddress)					
+				requestNumber += 1
+				time.sleep(0)
 		print("chega")
 		package = createPackage(0, 0, 1, "nada")
 		serverSocket.sendto(package.encode(), clientAddress)
